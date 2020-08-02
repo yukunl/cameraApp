5f69152a96a1b481ed7ab01f2e7dd94e9619af14
dongyu9817 <43794945+dongyu9817@users.noreply.github.com>
2020-08-02 13:16:50 -0500

    Combine Accelerometer with Camera

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -8,8 +8,37 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private static final String TAG = "MainActivity";
+    private SensorManager sensorManager;
+    TextView xvalue, yvalue, zvalue;
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
+    Sensor accelerometer;
 
     // ImageView imageView;
     @Override
@@ -17,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button backCamera = findViewById(R.id.facecamera);
+        Button backCamera = findViewById(R.id.backcamera);
 
         backCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,13 +59,199 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null) {
+            sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        mChart = (LineChart) findViewById(R.id.accChart);
+        mChart.getDescription().setEnabled(true);
+        mChart.getDescription().setText("Real Time Accelerometer Data Plot");
+        mChart.setTouchEnabled(false);
+        mChart.setDragEnabled(false);
+        mChart.setDrawGridBackground(false);
+        mChart.setPinchZoom(false);
+
+
+        mChart.setBackgroundColor(Color.WHITE);
+        LineData data = new LineData();
+        data.setValueTextColor(Color.BLACK);
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.BLACK);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.BLACK);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.BLACK);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMaximum(10f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+
+        feedMultiple();
+
+        //for textview data
+        xvalue = (TextView) findViewById(R.id.xValue);
+        yvalue = (TextView) findViewById(R.id.yValue);
+        zvalue = (TextView) findViewById(R.id.zValue);
+
+        startPlot ();
 
     }
+
+    private void startPlot () {
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    plotData = true;
+                    try {
+                        Thread.sleep( 10);
+                    }catch ( InterruptedException e ) { e.printStackTrace();}
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        Log.d("Accelerometer", "onSensorChanged x: " + event.values[0] + "Y: " + event.values[1] + "Z: " + event.values[2]);
+        xvalue.setText("X value: " + event.values[0]);
+        yvalue.setText("Y value: " + event.values[1]);
+        zvalue.setText("Z value: " + event.values[2]);
+
+        if (plotData) {
+            addEntry(event);
+            plotData = false;
+        }
+    }
+
+    private void addEntry(SensorEvent event) {
+
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), event.values[0] + 5), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(150);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+    }
+
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(Color.MAGENTA);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    plotData = true;
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        sensorManager.unregisterListener(this);
+        thread.interrupt();
+        super.onDestroy();
+    }
+
 //    @Override
 //    protected void onActivityResult(int requestCode, int resultCode, Intent data){
 //        super.onActivityResult(requestCode, resultCode, data);
 //        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
 //       // imageView.setImageBitmap(bitmap);
 //    }
+
+
 }
